927ff8ccede9eb0266a3ad8290541ad933cdfd9d
Lucas Firmo <lucasmfir@gmail.com>
2021-04-13 00:20:32 -0300

    posts tests

--- lib/blog/posts.ex
+++ lib/blog/posts.ex
@@ -3,10 +3,18 @@ defmodule Blog.Posts do
 
   import Ecto.Query
 
-  def create(params, user_id) do
+  def create(%{"title" => _title, "content" => _content} = params, user_id) do
     params
     |> Map.put("user_id", user_id)
     |> Post.changeset()
     |> Repo.insert()
+  end
+
+  def create(%{"title" => _}, _) do
+    {:error, "\"content\" is required"}
+  end
+
+  def create(%{"content" => _}, _) do
+    {:error, "\"title\" is required"}
   end
 end
---
+++ test/blog/posts_test.exs
@@ -0,0 +1,40 @@
+defmodule Blog.PostsTest do
+  use Blog.DataCase, async: true
+
+  alias Blog.{Post, Posts, Token}
+
+  @default_user_params %{
+    "display_name" => "user name",
+    "email" => "user@email.com",
+    "password" => "123456",
+    "image" => "https://imagebucket.com/1"
+  }
+
+  @default_post_params %{
+    "title" => "post title",
+    "content" => "Bacon ipsum dolor amet spare ribs prosciutto pork chop pork belly."
+  }
+
+  describe "create/1" do
+    setup do
+      {:ok, _token, %{"user_id" => user_id}} = Blog.create_user(@default_user_params)
+
+      {:ok, user_id: user_id}
+    end
+
+    test "when params are valid, it creates a post", %{user_id: user_id} do
+      assert {:ok, %Post{} = post} = Posts.create(@default_post_params, user_id)
+      assert @default_post_params["title"] == post.title
+      assert @default_post_params["content"] == post.content
+      assert user_id == post.user_id
+    end
+
+    test "when params are missing, it returns an error", %{user_id: user_id} do
+      missing_title_params = Map.drop(@default_post_params, ["title"])
+      missing_content_params = Map.drop(@default_post_params, ["content"])
+
+      assert {:error, "\"title\" is required"} = Posts.create(missing_title_params, user_id)
+      assert {:error, "\"content\" is required"} = Posts.create(missing_content_params, user_id)
+    end
+  end
+end
